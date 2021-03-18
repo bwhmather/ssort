297ed8b409f3f577b892f3f1f800d04058f775bc
Ben Mather <bwhmather@bwhmather.com>
2021-03-18 17:39:29 +0000

    Add sample script

---
+++ example.py
@@ -0,0 +1,27 @@
+import something
+import thing as something_else
+
+# Comment describing A
+a = something
+
+call();llac()
+
+def function():
+    """
+    docs
+    """
+    return _function()
+class B:
+    def method():
+        return function()
+
+def _function():
+    pass
+
+def recursive():
+    return recursive()
+
+@something(2, kwarg=3)
+def decorated(arg, *args, kwarg, **kwargs):
+    pass
+
