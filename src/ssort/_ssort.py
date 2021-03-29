759d8d4fe224fab9af67f280bb4e7dddcb8b7b42
Ben Mather <bwhmather@bwhmather.com>
2021-03-29 21:48:25 +0100

    Return sorted string instead of printing it

--- src/ssort/_ssort.py
+++ src/ssort/_ssort.py
@@ -56,8 +56,6 @@ def ssort(text, *, filename="<unknown>"):
 
     sorted_statements = sort(statement_dependencies)
 
-    print(
-        "\n".join(
-            statement_texts[statement] for statement in sorted_statements
-        )
+    return "\n".join(
+        statement_texts[statement] for statement in sorted_statements
     )
