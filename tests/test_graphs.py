2061fe5d774b3a13201674567c4e128c872c268e
Ben Mather <bwhmather@bwhmather.com>
2021-04-18 15:25:47 +0100

    Add tests to make sure that topological_sort is stable

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,3 +1,5 @@
+import random
+
 from ssort._graphs import Graph, topological_sort
 
 
@@ -44,3 +46,47 @@ def test_topological_sort_root():
     graph.add_dependency(3, 4)
 
     assert topological_sort(graph) == [4, 1, 2, 3]
+
+
+def test_topological_sort_tree():
+    graph = Graph()
+
+    graph.add_node(1)
+    graph.add_node(2)
+    graph.add_node(3)
+    graph.add_node(4)
+    graph.add_node(5)
+
+    graph.add_dependency(3, 2)
+    graph.add_dependency(5, 4)
+    graph.add_dependency(5, 3)
+
+    assert topological_sort(graph) == [1, 2, 3, 4, 5]
+
+
+def test_unconnected_stable():
+    nodes = list(range(1, 100))
+    random.shuffle(nodes)
+
+    graph = Graph()
+    for node in nodes:
+        graph.add_node(node)
+
+    assert topological_sort(graph) == nodes
+
+
+def test_random_stable():
+    nodes = list(range(100))
+    random.shuffle(nodes)
+
+    graph = Graph()
+
+    for node in nodes:
+        graph.add_node(node)
+
+    for _ in range(200):
+        src_index = random.randrange(1, 100)
+        tgt_index = random.randrange(src_index)
+        graph.add_dependency(nodes[src_index], nodes[tgt_index])
+
+    assert topological_sort(graph) == nodes
