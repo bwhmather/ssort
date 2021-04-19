209f00570d004c34332d8d39eb03ec2f4e1e44b0
Ben Mather <bwhmather@bwhmather.com>
2021-04-19 20:11:04 +0100

    Replace todo list with package description

--- src/ssort/__init__.py
+++ src/ssort/__init__.py
@@ -1,39 +1,5 @@
 """
-Steps:
-  - Identify chunks of file representing classes, declarations and functions.
-  - Parse each chunk to identify dependencies.
-  - Apply a stable topological sort.
-
-
-
-
-Identifying chunks:
-  - Comments should be attached to whatever comes after them.
-
-
-
-Weird things to handle:
-  - Unpacking resulting in multiple assignments.
-  - Semicolons resulting in multiple statements on same line.
-  - Comments attached to assignments.
-  - Redefinitions.
-  - Cycles.
-  - Immediate dependencies (e.g. decorators) vs deferred dependencies (e.g.
-    closed over variables)
-  - Deferred dependencies being re-evaluated part way through the file.
-  - Preventing re-ordering.
-  - Type annotations.
-
-Handling the difference between immediate and deferred deps:
-  - Treat all dependencies the same.
-  - Try to resolve dependencies immediately.
-  - If that fails, resolve dependencies at the very end.
-
-
-Extensions:
-  - Class members.
-  - Closures.
-
+The python source code statement sorter.
 """
 from ssort._ssort import ssort
 
