5fb7cd99687f747c31e1bc20a7b4c657e16043d5
Ben Mather <bwhmather@bwhmather.com>
2021-09-10 23:37:45 +0100

    Re-export ResolutionError

--- src/ssort/__init__.py
+++ src/ssort/__init__.py
@@ -1,7 +1,11 @@
 """
 The python source code statement sorter.
 """
+from ssort._exceptions import ResolutionError
 from ssort._ssort import ssort
+
+# Let linting tools know that we mean to re-export ResolutionError.
+assert ResolutionError is not None
 
 __version__ = "0.9.0"
 __all__ = ["ssort"]
