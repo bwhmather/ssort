b7f2cf43d24a86e12abc9c8bb07d212a7d514e7d
Ben Mather <bwhmather@bwhmather.com>
2022-02-13 15:01:12 +0000

    Remove unused function

--- src/ssort/_files.py
+++ src/ssort/_files.py
@@ -280,24 +280,3 @@ def gen_python_files(
             include_match = include.search(normalized_path) if include else True
             if include_match:
                 yield child
-
-
-def wrap_stream_for_windows(
-    f: io.TextIOWrapper,
-) -> Union[io.TextIOWrapper, "colorama.AnsiToWin32"]:
-    """
-    Wrap stream with colorama's wrap_stream so colors are shown on Windows.
-
-    If `colorama` is unavailable, the original stream is returned unmodified.
-    Otherwise, the `wrap_stream()` function determines whether the stream needs
-    to be wrapped for a Windows environment and will accordingly either return
-    an `AnsiToWin32` wrapper or the original stream.
-    """
-    try:
-        from colorama.initialise import wrap_stream
-    except ImportError:
-        return f
-    else:
-        # Set `strip=False` to avoid needing to modify test_express_diff_with_color.
-        return wrap_stream(f, convert=None, strip=False, autoreset=False, wrap=True)
-
