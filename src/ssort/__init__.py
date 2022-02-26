5705909a9c782b7c0d49802ab9b8a73e165fba5c
Ben Mather <bwhmather@bwhmather.com>
2022-02-26 09:27:27 +0000

    Release 0.11.0

--- src/ssort/__init__.py
+++ src/ssort/__init__.py
@@ -7,5 +7,5 @@ from ssort._ssort import ssort
 # Let linting tools know that we mean to re-export ResolutionError.
 assert ResolutionError is not None
 
-__version__ = "0.10.5"
+__version__ = "0.11.0"
 __all__ = ["ssort"]
