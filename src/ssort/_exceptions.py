1117d13a67673a842da8fdce3d8b958d0107dcff
Ben Mather <bwhmather@bwhmather.com>
2022-03-10 23:53:56 +0000

    Rewrite detect_encoding function as wrapper for equivalent in tokenize

--- src/ssort/_exceptions.py
+++ src/ssort/_exceptions.py
@@ -1,3 +1,9 @@
+class UnknownEncodingError(Exception):
+    def __init__(self, msg, *, encoding):
+        super().__init__(msg)
+        self.encoding = encoding
+
+
 class ResolutionError(Exception):
     def __init__(self, msg, *, unresolved):
         super().__init__(msg)
