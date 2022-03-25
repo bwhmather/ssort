0bb4611eb489ca30e560461657537965611d2b2a
jgberry <jgbcode@gmail.com>
2022-03-25 14:12:16 -0400

    Added ignored AST nodes to test_ast

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -8,15 +8,29 @@ import pytest
 
 from ssort._ast import iter_child_nodes
 
-_deprecated_node_types = {ast.AugLoad, ast.AugStore, ast.Param, ast.Suite}
+_deprecated_node_types = (ast.AugLoad, ast.AugStore, ast.Param, ast.Suite)
 
 if sys.version_info >= (3, 9):
-    _deprecated_node_types.update({ast.Index, ast.ExtSlice})
+    _deprecated_node_types += (ast.Index, ast.ExtSlice)
+
+_ignored_node_types = (
+    ast.expr_context,
+    ast.boolop,
+    ast.operator,
+    ast.unaryop,
+    ast.cmpop,
+)
 
 
-def _nodes(node_type: type[ast.AST] = ast.AST) -> Iterable[ast.AST]:
+def _nodes_types(
+    node_type: type[ast.AST] = ast.AST,
+) -> Iterable[type[ast.AST]]:
     # Skip deprecated AST nodes.
-    if node_type in _deprecated_node_types:
+    if issubclass(node_type, _deprecated_node_types):
+        return
+
+    # Skip ignored AST nodes.
+    if issubclass(node_type, _ignored_node_types):
         return
 
     subclasses = node_type.__subclasses__()
@@ -25,14 +39,15 @@ def _nodes(node_type: type[ast.AST] = ast.AST) -> Iterable[ast.AST]:
         # This is because AST base classes are used for categorical purposes
         # only and are not intended to be instantiated.
         for subclass in subclasses:
-            yield from _nodes(subclass)
+            yield from _nodes_types(subclass)
     else:
-        yield node_type()
+        yield node_type
 
 
 def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
-    nodes = list(_nodes())
-    ids = [type(node).__name__ for node in nodes]
+    node_types = list(_nodes_types())
+    nodes = [node_type() for node_type in node_types]
+    ids = [node_type.__name__ for node_type in node_types]
 
     metafunc.parametrize("node", nodes, ids=ids)
 
