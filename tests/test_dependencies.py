e4fe939aab029c8e94a12ce7f99e663df1e1ee66
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-04-25 11:58:47 +0200

    update tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -29,7 +29,10 @@ def test_dependencies_ordered_by_first_use():
     )
     c, a, b = statements = list(parse(source, filename="<unknown>"))
     graph = module_statements_graph(
-        statements, on_unresolved=_unreachable, on_wildcard_import=_unreachable
+        "<unknown>",
+        statements,
+        on_unresolved=_unreachable,
+        on_wildcard_import=_unreachable,
     )
 
     assert list(graph.dependencies[a]) == [b, c]
