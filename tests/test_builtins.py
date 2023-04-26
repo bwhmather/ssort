524d18b71b264bb3efe31e0000763b15a732e1a2
jgberry <jgbcode@gmail.com>
2023-04-26 17:07:06 -0400

    Additional builtin testing and fixes

--- tests/test_builtins.py
+++ tests/test_builtins.py
@@ -1,16 +1,70 @@
+import ast
 import builtins
+import os.path
+import subprocess
 
 from ssort._builtins import CLASS_BUILTINS, MODULE_BUILTINS
 
 
-def test_no_new_module_builtins():
-    # Checks that python hasn't introduced any new builtins that aren't in our
-    # list.
-    assert not set(builtins.__dict__) - MODULE_BUILTINS
+def _get_builtins():
+    return set(dir(builtins))
 
 
-def test_no_new_class_builtins():
+def _get_class_builtins():
     class Cls:
         builtins = locals()
 
-    assert not set(Cls.builtins) - {"builtins", *CLASS_BUILTINS}
+    return set(Cls.builtins)
+
+
+def _get_init_builtins():
+    import _builtins
+
+    return set(dir(_builtins))
+
+
+def _get_main_builtins():
+    return set(
+        ast.literal_eval(
+            subprocess.run(
+                ["python3", "_builtins"],
+                capture_output=True,
+                check=True,
+                cwd=os.path.dirname(__file__),
+                encoding="utf-8",
+            ).stdout
+        )
+    )
+
+
+def _get_module_builtins():
+    from _builtins import module
+
+    return set(dir(module))
+
+
+def test_no_new_builtins():
+    # Checks that python hasn't introduced any new builtins that aren't in our
+    # list.
+    missing = _get_builtins() - MODULE_BUILTINS
+    assert not missing
+
+
+def test_no_new_class_builtins():
+    missing = _get_class_builtins() - {"builtins", *CLASS_BUILTINS}
+    assert not missing
+
+
+def test_no_new_init_builtins():
+    missing = _get_init_builtins() - MODULE_BUILTINS
+    assert not missing
+
+
+def test_no_new_main_builtins():
+    missing = _get_main_builtins() - MODULE_BUILTINS
+    assert not missing
+
+
+def test_no_new_module_builtins():
+    missing = _get_module_builtins() - MODULE_BUILTINS
+    assert not missing
