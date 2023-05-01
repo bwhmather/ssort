b11811af844edb340ee9e254c511e2777a803817
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-05-01 09:19:17 +0200

    create current_working_dir to make testing easier, add more tests

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,68 +1,89 @@
+from pathlib import Path
+
 import pytest
 
-from ssort._files import find_project_root
-
-subdirs = [
-    (".",),
-    (".", "dir"),
-    ("dir", "dirA/B"),
-    ("dirA/B", "dir"),
-    ("a/a/a", "b/b/b"),
-    ("dir/a", "dir/b"),
-]
+from ssort._files import current_working_dir, find_project_root
 
 
-@pytest.fixture()
-def git(tmp_path):
-    root = tmp_path / "root"
-    (root / ".git").mkdir(parents=True)
-    return root
+def test_current_working_dir():
+    assert current_working_dir() == Path(".").resolve()
 
 
-@pytest.mark.parametrize("subdir", subdirs)
-def test_find_project_root_git(subdir, git):
-    print(subdir)
-    patterns = [git / sub for sub in subdir]
+class TestFindProjectRoot:
+    @pytest.fixture(
+        params=[
+            (".",),
+            (".", "dir"),
+            ("dir", "dirA/B"),
+            ("dirA/B", "dir"),
+            ("a/a/a", "b/b/b"),
+            ("dir/a", "dir/b"),
+        ]
+    )
+    def subdir(self, request):
+        return request.param
 
-    for p in patterns:
-        p.mkdir(parents=True, exist_ok=True)
+    @pytest.fixture()
+    def mock_current_working_dir(self, mocker, tmp_path):
+        return mocker.patch(
+            "ssort._files.current_working_dir", return_value=tmp_path / "root"
+        )
 
-    assert git == find_project_root(patterns)
+    @pytest.fixture()
+    def git(self, tmp_path):
+        root = tmp_path / "root"
+        (root / ".git").mkdir(parents=True)
+        return root
 
+    def test_find_project_root_git(
+        self, subdir, git, mock_current_working_dir
+    ):
+        print(subdir)
+        patterns = [git / sub for sub in subdir]
 
-@pytest.fixture()
-def pyproject(tmp_path):
-    root = tmp_path / "root"
-    root.mkdir()
-    (root / "pyproject.toml").touch()
-    return root
+        for p in patterns:
+            p.mkdir(parents=True, exist_ok=True)
 
+        assert git == find_project_root(patterns)
+        mock_current_working_dir.assert_called_once()
 
-@pytest.mark.parametrize("subdir", subdirs)
-def test_find_project_root_pyproject(subdir, pyproject):
-    patterns = [pyproject / sub for sub in subdir]
+    @pytest.fixture()
+    def pyproject(self, tmp_path):
+        root = tmp_path / "root"
+        root.mkdir()
+        (root / "pyproject.toml").touch()
+        return root
 
-    for p in patterns:
-        p.mkdir(parents=True, exist_ok=True)
+    def test_find_project_root_pyproject(
+        self, subdir, pyproject, mock_current_working_dir
+    ):
+        patterns = [pyproject / sub for sub in subdir]
 
-    assert pyproject == find_project_root(patterns)
+        for p in patterns:
+            p.mkdir(parents=True, exist_ok=True)
 
+        assert pyproject == find_project_root(patterns)
+        mock_current_working_dir.assert_called_once()
 
-@pytest.fixture()
-def neither(tmp_path):
-    root = tmp_path / "root"
-    root.mkdir()
-    return root
+    @pytest.fixture()
+    def neither(self, tmp_path):
+        root = tmp_path / "root"
+        root.mkdir()
+        return root
 
+    def test_find_project_root_neither(
+        self, subdir, neither, mocker, tmp_path
+    ):
+        mocker.patch(
+            "ssort._files.current_working_dir",
+            return_value=tmp_path / "root" / "dir",
+        )
+        patterns = [neither / sub for sub in subdir]
 
-@pytest.mark.parametrize("subdir", subdirs)
-def test_find_project_root_neither(subdir, neither):
-    patterns = [neither / sub for sub in subdir]
+        if all(s.startswith("dir/") for s in subdir):
+            neither = neither / "dir"
 
-    if all(s.startswith("dir/") for s in subdir):
-        neither = neither / "dir"
+        for p in patterns:
+            p.mkdir(parents=True, exist_ok=True)
 
-    for p in patterns:
-        p.mkdir(parents=True, exist_ok=True)
-
-    assert neither == find_project_root(patterns)
+        assert neither == find_project_root(patterns)
